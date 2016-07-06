db9a83cbd110add1517997ac62de06b8546556c7
Shradha Pulla and Charlie Ewel <pullashradha@gmail.com>
2016-07-06 14:57:29 -0700

    Make any unnecessary capital letters in the middle of a inputted phrase be lowercase in the result phrase

--- find-replace/Objects/FindReplace.cs
+++ find-replace/Objects/FindReplace.cs
@@ -11,10 +11,19 @@ namespace FindReplace.Objects
     {
       if (IsCaseSensitive)
       {
-        string partialMatched=inputPhrase.Replace(wordToReplace, newWord);
+        string partialMatched = inputPhrase.Replace(wordToReplace, newWord);
         if (PartialMatches)
         {
-          return partialMatched;
+          string[] finalReplacedArray = Regex.Split(partialMatched," ");
+          List<string> finalWords = new List<string> {};
+          foreach (string word in finalReplacedArray)
+          {
+            string firstletter=word.Substring(0,1);
+            string splitWord = word.Substring(1);
+            finalWords.Add(firstletter+splitWord.ToLower());
+          }
+          string finalPhrase=string.Join(" ",finalWords);
+          return finalPhrase;
         }
         else
         {
@@ -22,24 +31,43 @@ namespace FindReplace.Objects
           Regex characterInBack = new Regex(newWord + @"([a-zA-Z])");
           string frontReReplaced = characterInFront.Replace(partialMatched, "$1" + wordToReplace);
           string AllReReplaced= characterInBack.Replace(frontReReplaced, wordToReplace + "$1");
-          return AllReReplaced;
+          // return AllReReplaced;
+          string[] finalReplacedArray = Regex.Split(AllReReplaced," ");
+          List<string> finalWords = new List<string> {};
+          foreach (string word in finalReplacedArray)
+          {
+            string firstletter=word.Substring(0,1);
+            string splitWord = word.Substring(1);
+            finalWords.Add(firstletter+splitWord.ToLower());
+          }
+          string finalPhrase=string.Join(" ",finalWords);
+          return finalPhrase;
         }
       }
       else
       {
-        string firstReplaceLetter=wordToReplace.Substring(0,1);
-        string restOfReplaceWord=wordToReplace.Substring(1);
-        string UpperCaseReplaceWord=firstReplaceLetter.ToUpper()+restOfReplaceWord;
-        string LowerCaseReplaceWord=firstReplaceLetter.ToLower()+restOfReplaceWord;
-        string firstNewLetter=newWord.Substring(0,1);
-        string restOfNewWord=newWord.Substring(1);
-        string UpperCaseNewWord=firstNewLetter.ToUpper()+restOfNewWord;
-        string LowerCaseNewWord=firstNewLetter.ToLower()+restOfNewWord;
+        string firstReplaceLetter = wordToReplace.Substring(0,1);
+        string restOfReplaceWord = wordToReplace.Substring(1);
+        string UpperCaseReplaceWord = firstReplaceLetter.ToUpper()+restOfReplaceWord;
+        string LowerCaseReplaceWord = firstReplaceLetter.ToLower()+restOfReplaceWord;
+        string firstNewLetter = newWord.Substring(0,1);
+        string restOfNewWord = newWord.Substring(1);
+        string UpperCaseNewWord = firstNewLetter.ToUpper()+restOfNewWord;
+        string LowerCaseNewWord = firstNewLetter.ToLower()+restOfNewWord;
         string UpperCaseReplaced = inputPhrase.Replace(LowerCaseReplaceWord, LowerCaseNewWord);
         string FinalReplaced = UpperCaseReplaced.Replace(UpperCaseReplaceWord, UpperCaseNewWord);
         if (PartialMatches)
         {
-          return FinalReplaced;
+          string[] finalReplacedArray = Regex.Split(FinalReplaced," ");
+          List<string> finalWords = new List<string> {};
+          foreach (string word in finalReplacedArray)
+          {
+            string firstletter = word.Substring(0,1);
+            string splitWord = word.Substring(1);
+            finalWords.Add(firstletter + splitWord.ToLower());
+          }
+          string finalPhrase = string.Join(" ",finalWords);
+          return finalPhrase;
         }
         else
         {
@@ -51,7 +79,16 @@ namespace FindReplace.Objects
           string allFrontReReplaced = lowerCharacterInFront.Replace(upperFrontReReplaced, "$1" + LowerCaseReplaceWord);
           string upperBackReReplaced = upperCharacterInBack.Replace(allFrontReReplaced, UpperCaseReplaceWord + "$1");
           string AllReReplaced = lowerCharacterInBack.Replace(upperBackReReplaced, LowerCaseReplaceWord + "$1");
-          return AllReReplaced;
+          string[] finalReplacedArray = Regex.Split(AllReReplaced," ");
+          List<string> finalWords = new List<string> {};
+          foreach (string word in finalReplacedArray)
+          {
+            string firstletter = word.Substring(0,1);
+            string splitWord = word.Substring(1);
+            finalWords.Add(firstletter + splitWord.ToLower());
+          }
+          string finalPhrase = string.Join(" ",finalWords);
+          return finalPhrase;
         }
       }
     }
